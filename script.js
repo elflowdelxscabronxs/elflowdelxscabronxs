72acd840be16b8d86c6aeb92b45f6c58defb6178
FRANCISCO FERNANDEZ <franferz@gmail.com>
2024-08-20 14:34:14 -0300

    pequeñas modificaciones del modal

--- script.js
+++ script.js
@@ -1,53 +1,60 @@
 const videos = [
-  {
-    url: 'https://www.youtube.com/watch?v=B9Z50F1YMtQ',
-    artist: 'Lucía Vargas',
-    city: 'Cali',
-    country: 'Colombia',
-    video: 'Por Aguantar',
-  },
-  {
-    url: 'https://www.youtube.com/watch?v=T8g-LDSfRXY',
-    artist: 'Diana Avella',
-    city: 'Bogotá',
-    country: 'Colombia',
-    video: 'Micro-documental',
-  },
-  {
-    url: 'https://www.youtube.com/watch?v=T_j-718lmfo',
-    artist: 'El Flow de las cabronas',
-    city: ['Bogotá', 'Cali', 'Medellín'],
-    country: 'Colombia',
-    video: 'El Flow de las cabronas (Fragmento Colombia)',
-  },
-  {
-    url: 'https://www.youtube.com/watch?v=yIXZNbvHunc',
-    artist: 'El Flow de las cabronas',
-    city: 'Santiago',
-    country: 'Chile',
-    video: 'El Flow de las cabronas (Fragmento Chile)',
-  },
-  {
-    url: 'https://www.youtube.com/watch?v=oR4plT-NqBY',
-    artist: 'Indómitamorfosis',
-    city: 'Valparaiso',
-    country: 'Chile',
-    video: 'Homenaje a Nicole Saavedra',
-  },
-  {
-    url: 'https://www.youtube.com/watch?v=QluevKchlR0',
-    artist: 'Krudxs Cubensi',
-    city: '',
-    country: ['Cuba', 'Estados Unidos', 'Argentina'],
-    video: 'Conversatorio',
-  },
-  {
-    url: 'https://www.youtube.com/watch?v=1CvxfzBFw_I',
-    artist: 'Indómitamorfosis',
-    city: 'Buenos Aires',
-    country: 'Argentina',
-    video: 'en Buenos Aires',
-  },
+    {
+        url: 'https://www.youtube.com/watch?v=B9Z50F1YMtQ',
+        artist: 'Lucía Vargas',
+        city: 'Cali',
+        country: 'Colombia',
+        video: 'Por Aguantar',
+        id: "B9Z50F1YMtQ",
+    },
+    {
+        url: 'https://www.youtube.com/watch?v=T8g-LDSfRXY',
+        artist: 'Diana Avella',
+        city: 'Bogotá',
+        country: 'Colombia',
+        video: 'Micro-documental',
+        id: "T8g-LDSfRXY",
+    },
+    {
+        url: 'https://www.youtube.com/watch?v=T_j-718lmfo',
+        artist: 'El Flow de las cabronas',
+        city: ['Bogotá', 'Cali', 'Medellín'],
+        country: 'Colombia',
+        video: 'El Flow de las cabronas (Fragmento Colombia)',
+        id: "T_j-718lmfo",
+    },
+    {
+        url: 'https://www.youtube.com/watch?v=yIXZNbvHunc',
+        artist: 'El Flow de las cabronas',
+        city: 'Santiago',
+        country: 'Chile',
+        video: 'El Flow de las cabronas (Fragmento Chile)',
+        id: "yIXZNbvHunc",
+    },
+    {
+        url: 'https://www.youtube.com/watch?v=oR4plT-NqBY',
+        artist: 'Indómitamorfosis',
+        city: 'Valparaiso',
+        country: 'Chile',
+        video: 'Homenaje a Nicole Saavedra',
+        id: "oR4plT-NqBY",
+    },
+    {
+        url: 'https://www.youtube.com/watch?v=QluevKchlR0',
+        artist: 'Krudxs Cubensi',
+        city: '',
+        country: ['Cuba', 'Estados Unidos', 'Argentina'],
+        video: 'Conversatorio',
+        id: "QluevKchlR0",
+    },
+    {
+        url: 'https://www.youtube.com/watch?v=1CvxfzBFw_I',
+        artist: 'Indómitamorfosis',
+        city: 'Buenos Aires',
+        country: 'Argentina',
+        video: 'en Buenos Aires',
+        id: "1CvxfzBFw_I",
+    },
 ];
 
 const map = document.querySelector('.map');
@@ -55,39 +62,44 @@ const reproductor = document.querySelector('.rep-outer');
 const interior = document.querySelector('.rep-inner');
 
 const handleMapClick = () => {
-  interior.innerHTML = '';
-  reproductor.classList.add('open');
-  const videoList = document.createElement('ul');
-  interior.appendChild(videoList);
-  videos.forEach((video) => {
-    const listItem = document.createElement('li');
-    const videoBtn = document.createElement('button');
-    videoBtn.innerText = video.video;
-    listItem.appendChild(videoBtn);
-    videoList.appendChild(listItem);
-    // hacer que los links lleven a el video correspondiente embebido
-    videoBtn.addEventListener('click', handleButtonClick);
-  });
+    interior.innerHTML = '';
+    reproductor.classList.add('open');
+    const videoList = document.createElement('ul');
+    interior.appendChild(videoList);
+    videos.forEach((video) => {
+        const listItem = document.createElement('li');
+        const videoBtn = document.createElement('button');
+        videoBtn.innerText = video.video;
+        listItem.appendChild(videoBtn);
+        videoList.appendChild(listItem);
+        videoBtn.addEventListener('click', handleButtonClick);
+    });
 };
 
 const closeRep = () => {
-  reproductor.classList.remove('open');
+    reproductor.classList.remove('open');
+};
+
+const handleRetroceder = () => {
+
 };
 
 const handleButtonClick = (e) => {
-  const retroceder = document.createElement('button');
-  retroceder.innerText = 'Atras';
-  interior.innerHTML = '';
-  interior.appendChild(retroceder);
-  interior.appendChild();
+    const retroceder = document.createElement('button');
+    retroceder.innerText = 'Atras';
+    retroceder.addEventListener("click", handleMapClick)
+    console.log(e.target.innerText)
+    const videoRep = document.createElement("iframe");
+    videoRep.src = `https://www.youtube.com/embed/` + videos.find((video)=>e.target.innerText === video.video).id
+    interior.innerHTML = '';
+    interior.appendChild(retroceder);
+    interior.appendChild(videoRep);
 };
 
 map.addEventListener('click', handleMapClick);
 
 reproductor.addEventListener('click', (event) => {
-  console.log([...event.target.classList].includes('rep-outer'));
-
-  if ([...event.target.classList].includes('rep-outer')) {
-    closeRep();
-  }
+    if ([...event.target.classList].includes('rep-outer')) {
+        closeRep();
+    }
 });
